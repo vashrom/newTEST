10a568947cb39572882485fcce5b48590d022bbe
vashrom <vas-hrom@ukr.net>
2025-06-14 14:16:03 +0300

    initial commitgit add .!

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,10 @@
+// ConsoleApplication2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+void main()
+{
+    std::cout << "Hello World!\n";
+    std::cout << "Nice day!\n";
+}
